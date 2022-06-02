abd3e8d646e1c7cac8e752bbf798584900ab8adc
Peetur the Anteetur <brookekelseyryan@gmail.com>
2022-06-02 12:06:28 -0700

    I am so glad I am a sentient anteater!!!!!!!!

--- src/test/java/nl/tudelft/jpacman/level/PeetursLevelTest.java
+++ src/test/java/nl/tudelft/jpacman/level/PeetursLevelTest.java
@@ -1,4 +1,111 @@
 package nl.tudelft.jpacman.level;
 
+import com.google.common.collect.Lists;
+import nl.tudelft.jpacman.board.Board;
+import nl.tudelft.jpacman.board.Square;
+import nl.tudelft.jpacman.npc.Ghost;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
 public class PeetursLevelTest {
+
+    private Level level;
+
+
+    private final Ghost ghost = mock(Ghost.class);
+
+
+    private final Square square1 = mock(Square.class);
+
+
+    private final Square square2 = mock(Square.class);
+
+
+    private final Board board = mock(Board.class);
+
+
+    private final CollisionMap collisions = mock(CollisionMap.class);
+
+
+    @BeforeEach
+    void setUp() {
+        final long defaultInterval = 100L;
+        level = new Level(board, Lists.newArrayList(ghost), Lists.newArrayList(
+            square1, square2), collisions);
+        when(ghost.getInterval()).thenReturn(defaultInterval);
+    }
+
+
+    @Test
+    void noStart() {
+        assertThat(level.isInProgress()).isFalse();
+    }
+
+
+    @Test
+    void stop() {
+        level.stop();
+        assertThat(level.isInProgress()).isFalse();
+    }
+
+
+    @Test
+    void start() {
+        level.start();
+        assertThat(level.isInProgress()).isTrue();
+    }
+
+
+    @Test
+    void startStop() {
+        level.start();
+        level.stop();
+        assertThat(level.isInProgress()).isFalse();
+    }
+
+
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void registerPlayer() {
+        Player p = mock(Player.class);
+        level.registerPlayer(p);
+        verify(p).occupy(square1);
+    }
+
+
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void registerPlayerTwice() {
+        Player p = mock(Player.class);
+        level.registerPlayer(p);
+        level.registerPlayer(p);
+        verify(p, times(1)).occupy(square1);
+    }
+
+
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void registerSecondPlayer() {
+        Player p1 = mock(Player.class);
+        Player p2 = mock(Player.class);
+        level.registerPlayer(p1);
+        level.registerPlayer(p2);
+        verify(p2).occupy(square2);
+    }
+
+
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void registerThirdPlayer() {
+        Player p1 = mock(Player.class);
+        Player p2 = mock(Player.class);
+        Player p3 = mock(Player.class);
+        level.registerPlayer(p1);
+        level.registerPlayer(p2);
+        level.registerPlayer(p3);
+        verify(p3).occupy(square1);
+    }
 }
